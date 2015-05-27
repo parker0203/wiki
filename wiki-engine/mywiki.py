1fecd9256bef282a4ab14ada7e7418d4b3ba56a5
parker0203 <parker0203@gmail.com>
2015-05-27 01:45:19 -0700

    markdown all block instead of lines

--- wiki-engine/mywiki.py
+++ wiki-engine/mywiki.py
@@ -21,9 +21,12 @@ class git_md(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'html'
         data = reader.read_md(path.md_path + "git.md")
+        line2 = ""
         for line in data:
-            html = markdown.markdown(line)
-            self.response.write(html)
+            line2 += line
+            line2 += "\r"
+        html = markdown.markdown(line2)
+        self.response.write(html)
         
 
 app = webapp2.WSGIApplication([
